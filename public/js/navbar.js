1118fcd08fe39f4283519ecc296a43e654244020
ae <aaemijoh@gmail.com>
2024-12-27 11:11:51 -0700

    Update and rename navbar to navbar.js

---
+++ public/js/navbar.js
@@ -0,0 +1,10 @@
+document.getElementById('navbar').innerHTML = `
+    <ul>
+       <li><a href="../../index.html">home</a></li>
+       <li><a href="about.html">webmaster</a></li>
+       <li><a href="art.html">art</a></li>
+       <li><a href="photoshome.html">photos</a></li>
+       <li><a href="diary.html">diary</a></li>
+       <li><a href="interesting.html">>ᴗ<</a></li>
+   </ul>
+`;
